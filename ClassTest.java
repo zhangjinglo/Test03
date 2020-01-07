6a509712f3b9421d596eaa1fdb916a52d8edad78
zhangjingle <57256889+zhangjinglo@users.noreply.github.com>
2020-01-07 23:04:31 +0800

    Create ClassTest.java

---
+++ ClassTest.java
@@ -0,0 +1,19 @@
+package WestTwoTest03;
+
+public class ClassTest {
+    private int cno;
+    private String cname;
+
+    public ClassTest(int cno,String cname){
+        this.cno=cno;
+        this.cname=cname;
+    }
+
+    public int getCno() {
+        return cno;
+    }
+
+    public String getCname() {
+        return cname;
+    }
+}
